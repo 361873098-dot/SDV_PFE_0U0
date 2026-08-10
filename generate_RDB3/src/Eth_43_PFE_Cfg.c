17ecb59c1449c15d00a4d2803d520d36b6a35470
weizhichun <361873098@qq.com>
2026-08-10 11:15:46 +0800

    add can module

--- generate_RDB3/src/Eth_43_PFE_Cfg.c
+++ generate_RDB3/src/Eth_43_PFE_Cfg.c
@@ -4,7 +4,7 @@
 *
 *   @brief   AUTOSAR Eth_43_PFE driver Link-Time configuration file
 *   @details File containing the configuration for the Ethernet driver
-*   @note    This file was generated on 2026-08-07.
+*   @note    This file was generated on 2026-08-10.
 *
 *   @addtogroup ETH_43_PFE_DRIVER_CONFIGURATION
 *   @{
